b0eb83533a6adb6682d98a1aa09e45728f672227
B533P-17\533 <pellthomas@hotmail.com>
2018-08-27 16:55:39 -0400

    Modifications

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    CharacterController characterController;
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(horizontal, 0, vertical);
+        characterController.SimpleMove(mouvement * Time.deltaTime * 500);
+	}
+}
